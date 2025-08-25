8fc5bfb223018dbeb762e9791923ddb5b5f98424
cdstk <cdstkemail@gmail.com>
2025-08-24 19:30:17 -0500

    GitIgnore local configs + Fix PotionCore Redirect

--- src/main/java/lycanitestweaks/mixin/potioncore/PotionCoreEventHandlerMixin.java
+++ src/main/java/lycanitestweaks/mixin/potioncore/PotionCoreEventHandlerMixin.java
@@ -1,13 +1,14 @@
 package lycanitestweaks.mixin.potioncore;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.lycanitesmobs.core.entity.BaseCreatureEntity;
 import com.tmtravlr.potioncore.PotionCoreEventHandler;
 import lycanitestweaks.handlers.ForgeConfigHandler;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PotionCoreEventHandler.class)
 public abstract class PotionCoreEventHandlerMixin {
@@ -17,16 +18,19 @@ public abstract class PotionCoreEventHandlerMixin {
      * Based on fix in Bounceable by fonnymunkey
      * 1. Maintain "bouncing" behavior of flying mobs
      * 2. Restore motionY jumping abilities of mobs
+     *
      */
-    @Redirect(
+    @ModifyExpressionValue(
             method = "onLivingJump",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/entity/EntityLivingBase;motionY:D", ordinal = 2)
+            at = @At(value = "INVOKE", target = "Ljava/lang/Math;sqrt(D)D"),
+            remap = false
     )
-    private static void lycanitesTweaks_potionCoreEventHandler_onLivingJump(EntityLivingBase instance, double value) {
-        if(instance instanceof BaseCreatureEntity ||
-                (ForgeConfigHandler.integrationConfig.fixAllMobsPotionCoreJump && instance instanceof EntityLiving)) {
-            instance.motionY = Math.max(value, instance.motionY);
+    private static double lycanitesTweaks_potionCoreEventHandler_onLivingJump(double potionCoreSetValue, LivingEvent.LivingJumpEvent event) {
+        EntityLivingBase entityLivingBase = event.getEntityLiving();
+        if(entityLivingBase instanceof BaseCreatureEntity
+                || (ForgeConfigHandler.integrationConfig.fixAllMobsPotionCoreJump && entityLivingBase instanceof EntityLiving)) {
+            return Math.max(potionCoreSetValue, entityLivingBase.motionY);
         }
-        else instance.motionY = value;
+        return potionCoreSetValue;
     }
 }
