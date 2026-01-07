481890f3b5737abedc07ab34a72c9b852ce30f7e
cdstk <cdstkemail@gmail.com>
2026-01-06 19:52:15 -0600

    Bump 1.0.13

--- src/main/java/lycanitestweaks/LycanitesTweaks.java
+++ src/main/java/lycanitestweaks/LycanitesTweaks.java
@@ -36,7 +36,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = LycanitesTweaks.MODID, version = LycanitesTweaks.VERSION, name = LycanitesTweaks.NAME, dependencies = "required-after:fermiumbooter@[1.3.0,);required-after:lycanitesmobs")
 public class LycanitesTweaks {
     public static final String MODID = "lycanitestweaks";
-    public static final String VERSION = "1.0.12";
+    public static final String VERSION = "1.0.13";
     public static final String NAME = "LycanitesTweaks";
     public static final Logger LOGGER = LogManager.getLogger();
 	
