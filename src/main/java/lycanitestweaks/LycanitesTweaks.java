710de7bafb332409706f52c4384d6adb204e9ba6
cdstk <cdstkemail@gmail.com>
2026-02-26 21:06:02 -0600

    Older FM

--- src/main/java/lycanitestweaks/LycanitesTweaks.java
+++ src/main/java/lycanitestweaks/LycanitesTweaks.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = LycanitesTweaks.MODID, version = LycanitesTweaks.VERSION, name = LycanitesTweaks.NAME, dependencies = "required-after:fermiumbooter@[1.3.0,);required-after:lycanitesmobs")
+@Mod(modid = LycanitesTweaks.MODID, version = LycanitesTweaks.VERSION, name = LycanitesTweaks.NAME, dependencies = "required-after:fermiumbooter;required-after:lycanitesmobs")
 public class LycanitesTweaks {
     public static final String MODID = "lycanitestweaks";
     public static final String VERSION = "1.0.12-LITE";
